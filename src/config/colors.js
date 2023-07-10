aba0e445b0803d6373f8193d882489d16ae0ddfb
Igor <jadilsonigor@gmail.com>
2023-07-10 18:37:17 -0300

    'Initial'

---
+++ src/config/colors.js
@@ -0,0 +1,7 @@
+export const primaryColor = '#C3073F';
+export const primaryDarkColor = '#1A1A1D';
+
+export const successColor = '#01F9796';
+export const infoColor = '#01F9796';
+export const errorColor = '#F2AF29';
+export const warningColor = '#F2AF29';
---
+++ src/pages/Login/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+import { isEmail } from 'validator';
+import { useDispatch, useSelector } from 'react-redux';
+import { get } from 'lodash';
+
+import { Container } from '../../styles/GlobalStyles';
+import { Form } from './styled';
+import * as actions from '../../store/modules/auth/actions';
+import Loading from '../../components/Loading';
+
+export default function Login(props) {
+  const dispatch = useDispatch();
+
+  const prevPath = get(props, 'location.state.prevPath', '/');
+
+  const isLoading = useSelector((state) => state.auth.isLoading);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let formErrors = false;
+
+    if (!isEmail(email)) {
+      // eslint-disable-next-line
+      formErrors = true;
+      toast.error('Email inválido');
+    }
+
+    if (password.length < 6 || password.length > 50) {
+      // eslint-disable-next-line
+      formErrors = true;
+      toast.error('Senha inválida');
+    }
+
+    if (formErrors);
+
+    dispatch(actions.loginRequest({ email, password, prevPath }));
+  };
+
+  return (
+    <Container>
+      <Loading isLoading={isLoading} />
+      <h1>Login</h1>
+
+      <Form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Seu email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Sua senha"
+        />
+
+        <button type="submit">Acessar</button>
+      </Form>
+    </Container>
+  );
+}
